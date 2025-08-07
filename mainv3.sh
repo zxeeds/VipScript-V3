d29134d3f4a900eebc3c93a63b37fff96a443e6a
xknightgaming018 <xknightgaming018@gmail.com>
2025-08-07 09:00:59 +0000

    07-08-2025 v3

--- mainv3.sh
+++ mainv3.sh
@@ -346,7 +346,7 @@ function install_xray() {
 clear
 print_install "Core Xray 1.8.1 Latest Version"
 domainSock_dir="/run/xray";! [ -d $domainSock_dir ] && mkdir  $domainSock_dir
-chown www-data.www-data $domainSock_dir
+chown www-data:www-data $domainSock_dir
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version
 wget -O /etc/xray/config.json "${REPO}cfg/config.json" >/dev/null 2>&1
